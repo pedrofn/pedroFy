abdcfb83ae5ee78dfcdf503d0714f96f80c58a34
Pedro Nogueira <phzinho38@gmail.com>
2020-06-23 13:26:42 -0300

    finished styles

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,6 +1,26 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    postion: relative;
+    background: #FFF;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    padding: 15px;
+    box-shadow: 0 1px 4px 0 rgba(192, 208, 230, 0.8);
+    border-top: 20px solid rgba(230, 236, 245, 0.5);
+    cursor: grab;
+    
+
+    header {
+        postion: absolute;
+        top: -22px;
+        left: 15px;
+    }
+
+    p {
+        font-wright: 500;
+        line-height: 20px;
+    }
 
     img {
         width: 24px;
@@ -12,4 +32,11 @@ export const Container = styled.div`
 
 `;
 
-export const Label = styled.span``,
+export const Label = styled.span`
+    width: 10px;
+    height: 10px;
+    border-radius: 2px;
+    display: inline-block;
+    background: ${props => props.color};
+
+`;
--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
     padding: 0 15px;
     height: 100%;
     flex: 0 0 320px;
+    opacity: ${props => props.done ? 0.6 : 1};
 
     & + div {
         border-left: 1px solid rgba(0, 0, 0, 0.05);
@@ -32,6 +33,10 @@ export const Container = styled.div`
             cursor: pointer;
 
         }
+    }
+
+    ul {
+        margin-top: 30px;
     }
 
 `;
